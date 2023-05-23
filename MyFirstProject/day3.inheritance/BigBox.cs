820a40c91bfe55d409c7e1f56cc91d9b4a5d3779
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-05-22 23:46:12 -0700

    Finished lesson on inheritance

---
+++ MyFirstProject/day3.inheritance/BigBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.day3.inheritance
+{
+    internal class BigBox : Box
+    {
+        public void CalculateVolume(int length, int width, int height)
+        {
+            Console.WriteLine("Volume: " + (length * width * height));
+        }
+    }
+}
---
+++ MyFirstProject/day3.inheritance/TestBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject.day3.inheritance
+{
+    internal class TestBox
+    {
+        static void Main()
+        {
+            Box fedex = new Box();
+            fedex.CalculateArea(4,5);
+
+            BigBox ups = new BigBox();
+            ups.CalculateArea(3, 4);
+            ups.CalculateVolume(3, 4, 5);        }
+    }
+}
